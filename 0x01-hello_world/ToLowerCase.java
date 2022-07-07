1b669572f1cddf3213701ff20f7dbf6040c8c52f
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-07 20:58:47 -0300

    lowercase

--- 0x01-hello_world/ToLowerCase.java
+++ 0x01-hello_world/ToLowerCase.java
@@ -1,6 +1,38 @@
 /**
  * to_LowerCase - Convert String to LowerCase
- *
  */
 public class ToLowerCase
 {
+	/*public class Solution
+	{
+		public String toLower(string S)
+		{
+			System.out.println(s);
+		}
+	}*/
+	static String toLower(String S)
+	{
+		char caracter;
+		String lower = "";
+		
+		int len = S.length();
+
+		for (int i = 0; i < len; i++)
+		{
+			caracter = S.charAt(i);
+			if (caracter >= 65 && caracter <= 90)
+				caracter += 32;
+			lower = lower + caracter;
+		}
+		return (lower);
+	}
+
+	public static void main(String[] args)
+	{
+		String reversed = "MAYUSCULA";
+
+		System.out.println(reversed);
+		reversed = toLower(reversed);
+		System.out.println(reversed);
+	}
+}
