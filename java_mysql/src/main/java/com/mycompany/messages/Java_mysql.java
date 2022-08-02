8063b31d8a8706d3672ddac2bd558e28f5149e8f
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-02 17:49:48 -0300

    java to mysql

--- java_mysql/src/main/java/com/mycompany/messages/Java_mysql.java
+++ java_mysql/src/main/java/com/mycompany/messages/Java_mysql.java
@@ -8,8 +8,7 @@ import java.sql.*;
 public class Java_mysql {
 
     public static void main(String[] args) throws SQLException {
-        DeleteReg(7);
-        ShowReg();
+        continue;
     }
     static void InsertReg(String message, String author) throws SQLException {
         try (Connection connect = DriverManager.getConnection(
